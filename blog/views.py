c9d8355b30c423083558f1cbe9aa0c1e871deb01
Wanderson Reis <wasare@gmail.com>
2023-06-28 10:57:57 -0300

    Introdução aos templates Django

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 
 def index(request):
     # return HttpResponse('Olá Django - index')
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 
 def ola(request):
     return HttpResponse('Olá django')
--- webapp/settings.py
+++ webapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Inclua no final do arquivo as duas configurações abaixo
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
