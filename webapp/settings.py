52a10248a07061b92ad632442028d01f17c49ae7
Wanderson Reis <wasare@gmail.com>
2023-08-23 00:07:27 -0300

    Configurado banco MySQL

--- webapp/settings.py
+++ webapp/settings.py
@@ -81,6 +81,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
