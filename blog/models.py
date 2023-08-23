16d4c72df7ebcb9b8be4e1fa30159287cb818e52
Wanderson Reis <wasare@gmail.com>
2023-08-23 01:08:04 -0300

    Inclui o model Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    body_text = models.TextField('Texto Principal')
+    pub_date = models.DateTimeField('Data Publicação', auto_now=True)
--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from blog.models import Post # Acrescentar
+
 def index(request):
     # return HttpResponse('Olá Django - index')
     # return render(request, 'index.html')
     return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 
-def ola(request):
+def ola(request): # Modificar
     # return HttpResponse('Olá django')
-    return render(request, 'home.html')
+    posts = Post.objects.all() # recupera todos os posts do banco de dados
+    context = {'posts_list': posts } # cria um dicionário com os dado
+    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
