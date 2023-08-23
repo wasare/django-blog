ac75eb5ac1e20a7d9324c79c50990abc05123991
Wanderson Reis <wasare@gmail.com>
2023-08-23 00:09:04 -0300

    Inclui o model Post no site admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Post # importa o model que vamos habilitar no admin site
+
+@admin.register(Post) # registar o model Post e habilita
+class PostAdmin(admin.ModelAdmin): # configura o model Post no admin site.
+    list_display = ('id', 'body_text', 'pub_date')
+    list_filter = ('pub_date',)
+
