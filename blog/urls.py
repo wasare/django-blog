1e9ed699b87800f89571b4caa1fd8b82c992da4a
Wanderson Reis <wasare@gmail.com>
2023-08-31 09:22:34 -0300

    Exemplos de views para o model Post

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from blog.views import index, ola
+from blog.views import index, ola, post_show, PostDetailView
 
 
 urlpatterns = [
     path('index/', index, name='index'),
     path('ola/', ola, name='ola' ),
+    path('posts/all', ola, name="posts_list"),
+    path('post/<int:post_id>', post_show, name="exibe_post"),
+    path('post/<int:pk>/show', PostDetailView.as_view(), name="post_detail"),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 from django.http import HttpResponse
@@ -15,3 +16,12 @@ def ola(request): # Modificar
     posts = Post.objects.all() # recupera todos os posts do banco de dados
     context = {'posts_list': posts } # cria um dicionário com os dado
     return render(request, 'posts.html', context) # renderiza o template e passa o contexto
+
+def post_show(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post/detail.html', {'post': post})
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post/detail.html'
+    context_object_name = 'post'
